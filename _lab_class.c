31bc20ed5a477b17331522e04d4313bd3e09549e
hynrusang <mashmalo1871@gmail.com>
2023-05-31 01:05:56 +0900

    Update _lab_class.c

--- _lab_class.c
+++ _lab_class.c
@@ -19,11 +19,18 @@ classes (Bank) {
 int f(Bank, balance) {
 	return this->money;
 }
+
+void f(Bank, display) {
+	printf("\n현재 잔고: %d\n", Bank$balance(this));
+}
+
 int f(Bank, deposit, int money) {
 	this->money += money;
 	printf("\n%d원이 입금되었습니다.\n", money);
+	Bank$display(this);
 	return this->money;
 }
+
 int f(Bank, withdraw, int money) {
 	if (this->money - money < 0) {
 		printf("\n잔액이 모자랍니다.\n");
@@ -32,10 +39,8 @@ int f(Bank, withdraw, int money) {
 		printf("\n%d원이 인출되었습니다.\n", money);
 		this->money -= money;
 	}
+	Bank$display(this);
 	return this->money;
-}
-void f(Bank, display) {
-	printf("\n현재 잔고: %d\n", Bank$balance(this));
 }
 
 constructor(Bank, int money) {
@@ -60,6 +65,7 @@ int f(MinusBank, withdraw, int money) {
 		printf("\n%d원이 인출되었습니다.\n", money);
 		this->money -= money;
 	}
+	Bank$display(this);
 	return this->money;
 }
 constructor(MinusBank, int money, double debt_p) {
@@ -72,33 +78,27 @@ destroyer (MinusBank) {
 }
 
 main() {
+	int testdata = 3000;
 	printf("c 객체 테스트 프로그램\n");
 	printf("\n시나리오: class Bank\n");
-	Bank* mybank = new (Bank, 2000);
 
-	Bank$display(mybank);
-	Bank$deposit(mybank, 3000);
-	Bank$display(mybank);
-	Bank$withdraw(mybank, 2000);
-	Bank$display(mybank);
-	Bank$withdraw(mybank, 2000);
-	Bank$display(mybank);
-	Bank$withdraw(mybank, 2000);
+	Bank* mybank = new (Bank, testdata);
 	Bank$display(mybank);
 
+	Bank$deposit(mybank, testdata);
+	Bank$withdraw(mybank, testdata);
+	Bank$withdraw(mybank, testdata);
+	Bank$withdraw(mybank, testdata);
 	delete (Bank, mybank);
+
 	printf("\n시나리오: class MinusBank extends Bank\n");
-	MinusBank* myminusbank = new (MinusBank, 2000, 10);
 
-	Bank$display(myminusbank);
-	Bank$deposit(myminusbank, 3000);
-	Bank$display(myminusbank);
-	MinusBank$withdraw(myminusbank, 2000);
-	Bank$display(myminusbank);
-	MinusBank$withdraw(myminusbank, 2000);
-	Bank$display(myminusbank);
-	MinusBank$withdraw(myminusbank, 2000);
+	MinusBank* myminusbank = new (MinusBank, testdata, 10);
 	Bank$display(myminusbank);
 
+	Bank$deposit(myminusbank, testdata);
+	MinusBank$withdraw(myminusbank, testdata);
+	MinusBank$withdraw(myminusbank, testdata);
+	MinusBank$withdraw(myminusbank, testdata);
 	delete (MinusBank, myminusbank);
 }
