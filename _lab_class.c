ceae5b2390265583947a75af35f50406d5d21cd6
hynrusang <mashmalo1871@gmail.com>
2023-05-31 00:23:04 +0900

    Update _lab_class.c

--- _lab_class.c
+++ _lab_class.c
@@ -4,13 +4,13 @@
 #define classes(_class_type) typedef struct _class_type _class_type; struct _class_type
 #define f(_class_type, _method, ...) _class_type##$##_method(_class_type* this, __VA_ARGS__) 
 
-#define constructor(_class_type, ...) _class_type* init_##_class_type(__VA_ARGS__)
+#define constructor(_class_type, ...) _class_type* new(_class_type, __VA_ARGS__)
 #define bind(_class_type) _class_type* this = malloc(sizeof(_class_type)); if (this == NULL) exit(1);
 #define destroyer(_class_type) void delete_##_class_type(_class_type* this)
 #define unbind free(this)
 
-#define new(_class_type, ...) init_##_class_type(__VA_ARGS__)
-#define delete(_class_type, _object) delete##_##_class_type(_object)
+#define new(_class_type, ...) new_##_class_type(__VA_ARGS__)
+#define delete(_class_type, _object) delete_##_class_type(_object)
 
 classes (Bank) {
 	int money;
