d50c8664528e85a40c1604321f1a9a6f2a1d6c3c
hynrusang <mashmalo1871@gmail.com>
2023-05-30 09:05:04 +0900

    Update Class.h

--- 1.0.0/Class.h
+++ 1.0.0/Class.h
@@ -10,28 +10,52 @@ Bank: Class create
 
 class (Bank) {
     int money;
-    void f(balance);
+    int f(balance);
+    int f(withdraw);
     void f(deposit);
 };
-void Bank_balance(Bank* this) {
-    printf("bank's balance: %d\n", this->money);
+
+def (Bank, balance) {
+    return this->money;
 }
-void Bank_deposit(Bank* this, int money) {
+
+def (Bank, withdraw, int money) {
+    if (this->money - money < 0) printf("\n\tcannnot withdraw %d$\n", money);
+    else {
+        printf("\n\t%d$ was withdraw\n", money);
+        this->money -= money;
+    }
+}
+
+def (Bank, deposit, int money) {
     this->money += money;
+    printf("\n\t%d$ has been deposited.\n", money);
 }
-constructor(Bank)
-    link(Bank, balance);
-    link(Bank, deposit);
-    this->money = 0;
+
+constructor (Bank, int money) {
+#define macro() Bank
+    def_this;
+    this(balance);
+    this(withdraw);
+    this(deposit);
+    this->money = money;
 }
 
 Bank: Class used
 
 int main() {
-    Bank* mybank = new (Bank);
-    mybank->balance(mybank);
-    mybank->deposit(mybank, 100);
-    mybank->balance(mybank);
+    printf("*****\t    Bank\t*****\n");
+    Bank* mybank = new (Bank, 20);
+
+    printf("\n\tbalance: %d$\n", mybank->balance(mybank));
+    mybank->deposit(mybank, 30);
+    printf("\n\tbalance: %d$\n", mybank->balance(mybank));
+    mybank->withdraw(mybank, 40);
+    printf("\n\tbalance: %d$\n", mybank->balance(mybank));
+    mybank->withdraw(mybank, 40);
+    printf("\n\tbalance: %d$\n", mybank->balance(mybank));
+
+    printf("\n***** Bank scenario end *****\n\n");
     free(mybank);
     return 0;
 }
