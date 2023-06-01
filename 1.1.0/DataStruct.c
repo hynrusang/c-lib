63c02014048a3be252e832bf011c4540e564dad6
hynrusang <mashmalo1871@gmail.com>
2023-06-01 17:32:58 +0900

    up

---
+++ 1.1.0/DataStruct.c
@@ -0,0 +1,122 @@
+#define key
+#ifdef key
+
+
+#define DEQUE int
+#include "DataStruct.h"
+
+main() {
+#ifdef STACK
+#define _data stack_data
+#define iterator stack_iterator
+	STACK _data;
+	Stack* stack = new (Stack);
+	StackElement* iterator;
+	stack->help();
+	printf("empty? %d\n\n", stack->is_empty(stack));
+
+	_data = 1;
+	for (; _data < 10; _data++) {
+		printf("push %d", _data);
+		stack->push(stack, _data);
+		printf("\ndatas = ( "); iterator = stack->first; for (; iterator != stack->last; iterator = iterator->next) printf("%d ", iterator->data);
+		printf(")\n");
+		printf("empty? %d\n\n", stack->is_empty(stack));
+	}
+	_data--;
+	printf("top %d", stack->top(stack));
+	printf("\ndatas = ( "); iterator = stack->first; for (; iterator != stack->last; iterator = iterator->next) printf("%d ", iterator->data);
+	printf(")\n");
+	printf("empty? %d\n\n", stack->is_empty(stack));
+	for (; 0 < _data; _data--) {
+		printf("pop %d", stack->pop(stack));
+		printf("\ndatas = ( "); iterator = stack->first; for (; iterator != stack->last; iterator = iterator->next) printf("%d ", iterator->data);
+		printf(")\n");
+		printf("empty? %d\n\n", stack->is_empty(stack));
+	}
+
+#endif
+
+#ifdef QUEUE
+#define _data queue_data
+#define iterator queue_iterator
+	QUEUE _data;
+	Queue* queue = new (Queue);
+	QueueElement* iterator;
+	queue->help();
+	printf("empty? %d\n\n", queue->is_empty(queue));
+
+	_data = 1;
+	for (; _data < 10; _data++) {
+		printf("enqueue %d", _data);
+		queue->enqueue(queue, _data);
+		printf("\ndatas = ( "); iterator = queue->first; for (; iterator != queue->last; iterator = iterator->next) printf("%d ", iterator->data);
+		printf(")\n");
+		printf("empty? %d\n\n", queue->is_empty(queue));
+	}
+	_data--;
+	printf("bottom %d", queue->bottom(queue));
+	printf("\ndatas = ( "); iterator = queue->first; for (; iterator != queue->last; iterator = iterator->next) printf("%d ", iterator->data);
+	printf(")\n");
+	printf("empty? %d\n\n", queue->is_empty(queue));
+	for (; 0 < _data; _data--) {
+		printf("dequeue %d", queue->dequeue(queue));
+		printf("\ndatas = ( "); iterator = queue->first; for (; iterator != queue->last; iterator = iterator->next) printf("%d ", iterator->data);
+		printf(")\n");
+		printf("empty? %d\n\n", queue->is_empty(queue));
+	}
+
+#endif
+
+#ifdef DEQUE
+#define _data deque_data
+#define iterator deque_iterator
+	DEQUE _data;
+	Deque* deque = new (Deque);
+	DequeElement* iterator;
+	deque->help();
+	printf("empty? %d\n\n", deque->is_empty(deque));
+
+	_data = 1;
+	for (; _data < 10; _data++) {
+		printf("push_back %d", _data);
+		deque->push_back(deque, _data);
+		printf("\ndatas = ( "); iterator = deque->first; for (; iterator != deque->last; iterator = iterator->next) printf("%d ", iterator->data);
+		printf(")\n");
+		printf("empty? %d\n\n", deque->is_empty(deque));
+	}
+	_data--;
+	printf("bottom %d", deque->bottom(deque));
+	printf("\ndatas = ( "); iterator = deque->first; for (; iterator != deque->last; iterator = iterator->next) printf("%d ", iterator->data);
+	printf(")\n");
+	printf("empty? %d\n\n", deque->is_empty(deque));
+	for (; 0 < _data; _data--) {
+		printf("pop_front %d", deque->pop_front(deque));
+		printf("\ndatas = ( "); iterator = deque->first; for (; iterator != deque->last; iterator = iterator->next) printf("%d ", iterator->data);
+		printf(")\n");
+		printf("empty? %d\n\n", deque->is_empty(deque));
+	}
+
+	_data = 1;
+	for (; _data < 10; _data++) {
+		printf("push_front %d", _data);
+		deque->push_front(deque, _data);
+		printf("\ndatas = ( "); iterator = deque->first; for (; iterator != deque->last; iterator = iterator->next) printf("%d ", iterator->data);
+		printf(")\n");
+		printf("empty? %d\n\n", deque->is_empty(deque));
+	}
+	_data--;
+	printf("top %d", deque->top(deque));
+	printf("\ndatas = ( "); iterator = deque->first; for (; iterator != deque->last; iterator = iterator->next) printf("%d ", iterator->data);
+	printf(")\n");
+	printf("empty? %d\n\n", deque->is_empty(deque));
+	for (; 0 < _data; _data--) {
+		printf("pop_back %d", deque->pop_back(deque));
+		printf("\ndatas = ( "); iterator = deque->first; for (; iterator != deque->last; iterator = iterator->next) printf("%d ", iterator->data);
+		printf(")\n");
+		printf("empty? %d\n\n", deque->is_empty(deque));
+	}
+
+#endif
+}
+#endif
---
+++ 1.1.0/DataStruct.h
@@ -0,0 +1,402 @@
+/*
+this source code is a source code that implements data structures such as Stack, Queue, Deque, and Tree in C language.
+When you use it,
+
+#define (STACK || QUEUE || DEQUE || TREE) (int || char || double || ...)
+#include "DataStruct.h href"
+
+You can create at the top of the C source file.
+For example, DataStructure.h is located in the same path as the C source file,
+will use Stack data structure, and if the data type is int,
+
+#define STACK int
+#include "DataStruct.h"
+
+You can write at the top of the C source.
+Author: Hynrusang
+*/
+#ifndef DATASTRUCTHEADER
+#define DATASTRUCTHEADER
+#define new(_class_type) _##_class_type##$init();
+#define __dynamic_error 1
+#define __data_struct_is_empty 2
+#include <stdio.h>
+#include <stdlib.h>
+/*
+throw Error with reason
+*/
+void _throw(int code) {
+	switch (code) {
+	case __dynamic_error:
+		printf("\nDynamic memory allocation failed (may be due to low memory)\n");
+		exit(code);
+	case __data_struct_is_empty:
+		printf("\nThis DataStructure is already blank.\n");
+		exit(code);
+	}
+}
+
+#ifdef STACK // using Stack
+
+// definition of Stack
+
+/*
+Stack Element(linked)
+*/
+typedef struct StackElement StackElement;
+struct StackElement {
+	STACK data;
+	StackElement* before;
+	StackElement* next;
+};
+/*
+controller(Stack Element)
+*/
+typedef struct Stack Stack;
+struct Stack {
+	StackElement* first;
+	StackElement* last;
+	void (*push)(Stack* this, STACK data);
+	STACK (*pop)(Stack* this);
+	STACK (*top)(Stack* this);
+	int (*is_empty)(Stack* this);
+	void (*help)();
+};
+
+// Stack util
+
+/*
+push data(type is STACK) to Stack(param)
+*/
+void _STACK$PUSH(Stack* stack, STACK data) {
+	stack->last->next = malloc(sizeof(StackElement));
+	if (stack->last->next == NULL) _throw(__dynamic_error);
+	else {
+		stack->last->data = data;
+		stack->last->next->before = stack->last;
+	}
+	stack->last = stack->last->next;
+}
+/*
+get Stack(param)'s last data and remove Stack(param)'s last data
+*/
+STACK _STACK$POP(Stack* stack) {
+	if (stack->first == stack->last) _throw(__data_struct_is_empty);
+	stack->last = stack->last->before;
+	free(stack->last->next);
+	return stack->last->data;
+}
+/*
+get Stack(param)'s last data
+*/
+STACK _STACK$TOP(Stack* stack) {
+	if (stack->first == stack->last) _throw(__data_struct_is_empty);
+	return stack->last->before->data;
+}
+/*
+get boolean Stack(param) is black
+*/
+int _STACK$IS_EMPTY(Stack* stack) {
+	return (stack->first == stack->last);
+}
+/*
+printf all available functions
+*/
+void _STACK$HELP() {
+	for (int i = 0; i < 30; i++) printf("*");
+	printf("\nall available struct list:\n");
+	printf("StackElement: struct stackElement\n");
+	printf("Stack: struct stack\n");
+
+	printf("\nall available functions list:\n");
+
+	printf("\nabout initialization:\n");
+	printf("new (Stack): Stack*\n");
+
+	printf("\nabout push:\n");
+	printf("stack->push(Stack*, STACK): void\n");
+
+	printf("\nabout pop:\n");
+	printf("stack->pop(Stack*): STACK\n");
+
+	printf("\nabout peek:\n");
+	printf("stack->top(Stack*): STACK\n");
+
+	printf("\nabout blank check:\n");
+	printf("stack->is_empty(Stack*): int\n");
+	for (int i = 0; i < 30; i++) printf("*");
+	printf("\n");
+}
+/*
+return init controller(Stack Element)
+*/
+Stack* _Stack$init() {
+	Stack* this = malloc(sizeof(Stack)); if (this == NULL) _throw(__dynamic_error);
+	this->first = malloc(sizeof(StackElement));
+	this->last = this->first;
+	this->push = _STACK$PUSH;
+	this->pop = _STACK$POP;
+	this->top = _STACK$TOP;
+	this->is_empty = _STACK$IS_EMPTY;
+	this->help = _STACK$HELP;
+	return this;
+}
+
+#endif
+
+#ifdef QUEUE // using Queue
+
+// definition of Queue
+
+/*
+Queue Element(linked)
+*/
+typedef struct QueueElement QueueElement;
+struct QueueElement {
+	QUEUE data;
+	QueueElement* before;
+	QueueElement* next;
+};
+/*
+controller(Queue Element)
+*/
+typedef struct Queue Queue;
+struct Queue {
+	QueueElement* first;
+	QueueElement* last;
+	void (*enqueue)(Queue* this, QUEUE data);
+	QUEUE (*dequeue)(Queue* this);
+	QUEUE (*bottom)(Queue* this);
+	int (*is_empty)(Queue* this);
+	void (*help)();
+};
+
+// Queue util
+
+/*
+push data(type is QUEUE) to Queue(param)
+*/
+void _QUEUE$ENQUEUE(Queue* queue, QUEUE data) {
+	queue->last->next = malloc(sizeof(QueueElement));
+	if (queue->last->next == NULL) _throw(__dynamic_error);
+	else {
+		queue->last->data = data;
+		queue->last->next->before = queue->last;
+	}
+	queue->last = queue->last->next;
+}
+/*
+get Queue(param)'s first data and remove Queue(param)'s first data
+*/
+QUEUE _QUEUE$DEQUEUE(Queue* queue) {
+	if (queue->first == queue->last) _throw(__data_struct_is_empty);
+	QUEUE temp = queue->first->data;
+	queue->first = queue->first->next;
+	free(queue->first->before);
+	return temp;
+}
+/*
+get Queue(param)'s last data
+*/
+QUEUE _QUEUE$BOTTOM(Queue* queue) {
+	if (queue->first == queue->last) _throw(__data_struct_is_empty);
+	return queue->first->data;
+}
+/*
+get boolean Queue(param) is black
+*/
+int _QUEUE$IS_EMPTY(Queue* queue) {
+	return (queue->first == queue->last);
+}
+/*
+printf all available functions
+*/
+void _QUEUE$HELP() {
+	for (int i = 0; i < 30; i++) printf("*");
+	printf("\nall available struct list:\n");
+	printf("QueueElement: struct queueElement\n");
+	printf("Queue: struct queue\n");
+
+	printf("\nall available functions list:\n");
+
+	printf("\nabout initialization:\n");
+	printf("new (Queue): Queue*\n");
+
+	printf("\nabout push:\n");
+	printf("queue->enqueue(Queue*, QUEUE): void\n");
+
+	printf("\nabout pop:\n");
+	printf("queue->dequeue(Queue*): QUEUE\n");
+
+	printf("\nabout peek:\n");
+	printf("queue->bottom(Queue*): QUEUE\n");
+
+	printf("\nabout blank check:\n");
+	printf("queue->is_empty(Queue*): int\n");
+	for (int i = 0; i < 30; i++) printf("*");
+	printf("\n");
+}
+/*
+return init controller(Queue Element)
+*/
+Queue* _Queue$init() {
+	Queue* temp = malloc(sizeof(Queue));
+	if (temp == NULL) _throw(__dynamic_error);
+	temp->first = malloc(sizeof(QueueElement));
+	temp->last = temp->first;
+	temp->enqueue = _QUEUE$ENQUEUE;
+	temp->dequeue = _QUEUE$DEQUEUE;
+	temp->bottom = _QUEUE$BOTTOM;
+	temp->is_empty = _QUEUE$IS_EMPTY;
+	temp->help = _QUEUE$HELP;
+	return temp;
+}
+
+#endif
+
+#ifdef DEQUE // using Deque
+
+// definition of Deque
+
+/*
+Deque Element(linked)
+*/
+typedef struct DequeElement DequeElement;
+struct DequeElement {
+	DEQUE data;
+	DequeElement* before;
+	DequeElement* next;
+};
+/*
+controller(Deque Element)
+*/
+typedef struct Deque Deque;
+struct Deque {
+	DequeElement* first;
+	DequeElement* last;
+	void (*push_front)(Deque* this, DEQUE data);
+	void (*push_back)(Deque* this, DEQUE data);
+	DEQUE (*pop_front)(Deque* this);
+	DEQUE (*pop_back)(Deque* this);
+	DEQUE (*top)(Deque* this);
+	DEQUE (*bottom)(Deque* this);
+	/*
+	get boolean Deque(param) is black
+	*/	
+	int (*is_empty)(Deque* this);
+	/*
+	printf all available functions
+	*/
+	void (*help)();
+};
+
+// Deque util
+
+/*
+push data(type is Deque) to Deque(param)'s first
+*/
+void _DEQUE$PUSH_FRONT(Deque* deque, DEQUE data) {
+	deque->first->before = malloc(sizeof(DequeElement));
+	if (deque->first->before == NULL) _throw(__dynamic_error);
+	else {
+		deque->first->before->next = deque->first;
+		deque->first = deque->first->before;
+		deque->first->data = data;
+	}
+}
+/*
+push data(type is Deque) to Deque(param)'s last
+*/
+void _DEQUE$PUSH_BACK(Deque* deque, DEQUE data) {
+	deque->last->next = malloc(sizeof(DequeElement));
+	if (deque->last->next == NULL) _throw(__dynamic_error);
+	else {
+		deque->last->data = data;
+		deque->last->next->before = deque->last;
+	}
+	deque->last = deque->last->next;
+}
+/*
+get Deque(param)'s first data and remove Deque(param)'s first data
+*/
+DEQUE _DEQUE$POP_FRONT(Deque* deque) {
+	if (deque->first == deque->last) _throw(__data_struct_is_empty);
+	DEQUE temp = deque->first->data;
+	deque->first = deque->first->next;
+	free(deque->first->before);
+	return temp;
+}
+/*
+get Deque(param)'s first data and remove Deque(param)'s last data
+*/
+DEQUE _DEQUE$POP_BACK(Deque* deque) {
+	if (deque->first == deque->last) _throw(__data_struct_is_empty);
+	deque->last = deque->last->before;
+	free(deque->last->next);
+	return deque->last->data;
+}
+/*
+get Deque(param)'s first data
+*/
+DEQUE _DEQUE$TOP(Deque* deque) {
+	if (deque->first == deque->last) _throw(__data_struct_is_empty);
+	return deque->last->before->data;
+}
+/*
+get Deque(param)'s last data
+*/
+DEQUE _DEQUE$BOTTOM(Deque* deque) {
+	if (deque->first == deque->last) _throw(__data_struct_is_empty);
+	return deque->first->data;
+}
+int _DEQUE$IS_EMPTY(Deque* deque) {
+	return (deque->first == deque->last);
+}
+void _DEQUE$HELP() {
+	for (int i = 0; i < 30; i++) printf("*");
+	printf("\nall available struct list:\n");
+	printf("DequeElement: struct dequeElement\n");
+	printf("Deque: struct deque\n");
+
+	printf("\nall available functions list:\n");
+
+	printf("\nabout initialization:\n");
+	printf("init(): Deque*\n");
+
+	printf("\nabout push:\n");
+	printf("push_front(Deque*, DEQUE): void\n");
+	printf("push_back(Deque*, DEQUE): void\n");
+
+	printf("\nabout pop:\n");
+	printf("pop_front(Deque*): DEQUE\n");
+	printf("pop_back(Deque*): DEQUE\n");
+
+	printf("\nabout peek:\n");
+	printf("top(Deque*): DEQUE\n");
+	printf("bottom(Deque*): DEQUE\n");
+
+	printf("\nabout blank check:\n");
+	printf("is_empty(Deque*): int\n");
+	for (int i = 0; i < 30; i++) printf("*");
+	printf("\n");
+}
+/*
+return init controller(Deque Element)
+*/
+Deque* _Deque$init() {
+	Deque* temp = malloc(sizeof(Deque));
+	if (temp == NULL) _throw(__dynamic_error);
+	temp->first = malloc(sizeof(DequeElement));
+	temp->last = temp->first;
+	temp->push_front = _DEQUE$PUSH_FRONT;
+	temp->push_back = _DEQUE$PUSH_BACK;
+	temp->pop_front = _DEQUE$POP_FRONT;
+	temp->pop_back = _DEQUE$POP_BACK;
+	temp->is_empty = _DEQUE$IS_EMPTY;
+	temp->help = _DEQUE$HELP;
+	return temp;
+}
+
+#endif
+
+#endif
